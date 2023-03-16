9ec7d4a5ec0dba40efd9f2e05b05448478b27493
LUCYOGUTU <amondilucy192@gmail.com>
2023-03-16 18:22:36 +0300

    feat: project set up

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => GreatPlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.lime,
+        ),
+        home: const PlacesListScreen(),
+      ),
+    );
+  }
+}
